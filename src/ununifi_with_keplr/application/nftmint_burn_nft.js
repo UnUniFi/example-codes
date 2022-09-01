957c7f2e46cfcf8aa8ae9cdccf94de84db69be3b
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-09-01 19:21:24 +0900

    feat: other msgs

---
+++ src/ununifi_with_keplr/application/nftmint_burn_nft.js
@@ -0,0 +1,24 @@
+function(instance, properties, context) {
+  const { sdk, currentAddressString, fetchAccount, createTx, loginWithKeplr, signWithKeplr, broadcastTx } = instance.data;
+  const chainId = instance.data.chainId;
+
+  const classId = properties.class_id;
+  const nftId = properties.nft_id;
+
+  const burnNft = async () => {
+    await loginWithKeplr(chainId);
+    const account = await fetchAccount(sdk, currentAddressString);
+    const msg = new ununificlient.proto.ununifi.nftmint.MsgBurnNFT({
+      sender: account.address,
+      class_id: classId,
+      nft_id: nftId,
+    });
+
+    const txBuilder = createTx(sdk, [msg], account);
+    const signedTxBuilder = await signWithKeplr(chainId, account, txBuilder);
+    const txHash = await broadcastTx(sdk, signedTxBuilder);
+    instance.publishState('tx_hash', txHash);
+    instance.triggerEvent('nft_burned');
+  };
+  burnNft();
+}
---
+++ src/ununifi_with_keplr/application/nftmint_update_token_supply_cap.js
@@ -0,0 +1,24 @@
+function(instance, properties, context) {
+  const { sdk, currentAddressString, fetchAccount, createTx, loginWithKeplr, signWithKeplr, broadcastTx } = instance.data;
+  const chainId = instance.data.chainId;
+
+  const classId = properties.class_id;
+  const tokenSupplyCap = properties.token_supply_cap;
+
+  const updateTokenSupplyCap = async () => {
+    await loginWithKeplr(chainId);
+    const account = await fetchAccount(sdk, currentAddressString);
+    const msg = new ununificlient.proto.ununifi.nftmint.MsgUpdateTokenSupplyCap({
+      sender: account.address,
+      class_id: classId,
+      token_supply_cap: Long.fromString(tokenSupplyCap),
+    });
+
+    const txBuilder = createTx(sdk, [msg], account);
+    const signedTxBuilder = await signWithKeplr(chainId, account, txBuilder);
+    const txHash = await broadcastTx(sdk, signedTxBuilder);
+    instance.publishState('tx_hash', txHash);
+    instance.triggerEvent('token_supply_cap_updated');
+  };
+  updateTokenSupplyCap();
+}
