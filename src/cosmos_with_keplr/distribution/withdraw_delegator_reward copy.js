cfe59e7978759f284a2b13d41f37124aac1cd47f
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-09-13 17:25:43 +0900

    feat: distribution

---
+++ src/cosmos_with_keplr/distribution/withdraw_delegator_reward copy.js
@@ -0,0 +1,23 @@
+function a(instance, properties, context) {
+  const { sdk, currentAddressString, fetchAccount, createTx, loginWithKeplr, signWithKeplr, broadcastTx } = instance.data;
+  const chainId = instance.data.chainId;
+
+  const validatorAddress = properties.validator_address;
+
+  const withdrawDelegatorReward = async () => {
+    await loginWithKeplr(chainId);
+    const account = await fetchAccount(sdk, currentAddressString);
+    const msg = new cosmosclient.proto.cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward({
+      delegator_address: account.address,
+      validator_address: validatorAddress,
+    });
+
+    const txBuilder = createTx(sdk, [msg], account);
+    const signedTxBuilder = await signWithKeplr(chainId, account, txBuilder);
+    const txHash = await broadcastTx(sdk, signedTxBuilder);
+    instance.publishState('tx_hash', txHash);
+    instance.triggerEvent('withdrew_delegator_reward');
+    console.log('txHash : ' + txHash);
+  };
+  withdrawDelegatorReward();
+}
---
+++ src/cosmos_with_keplr/distribution/withdraw_validator_commission.js
@@ -0,0 +1,20 @@
+function a(instance, properties, context) {
+  const { sdk, currentAddressString, fetchAccount, createTx, loginWithKeplr, signWithKeplr, broadcastTx } = instance.data;
+  const chainId = instance.data.chainId;
+
+  const withdrawValidatorCommission = async () => {
+    await loginWithKeplr(chainId);
+    const account = await fetchAccount(sdk, currentAddressString);
+    const msg = new cosmosclient.proto.cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission({
+      validator_address: account.address,
+    });
+
+    const txBuilder = createTx(sdk, [msg], account);
+    const signedTxBuilder = await signWithKeplr(chainId, account, txBuilder);
+    const txHash = await broadcastTx(sdk, signedTxBuilder);
+    instance.publishState('tx_hash', txHash);
+    instance.triggerEvent('withdrew_validator_commission');
+    console.log('txHash : ' + txHash);
+  };
+  withdrawValidatorCommission();
+}
