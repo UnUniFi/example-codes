17864cb433f265f36a4339521389ea1cdd087484
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-09-20 19:51:43 +0900

    feat: cosmos base

---
+++ src/cosmos_with_keplr/cosmos_broadcast_tx.bubble.js
@@ -0,0 +1,36 @@
+function(instance, properties, context) {
+    const { fromHexString, txBuilder, sdk } = instance.data;
+    const signedBodyHex = properties.signed_body_hex;
+    const signedAuthInfoHex = properties.signed_auth_info_hex;
+    const signature = properties.signature;
+
+    txBuilder.txRaw.auth_info_bytes = fromHexString(signedAuthInfoHex);
+    txBuilder.txRaw.body_bytes = fromHexString(signedBodyHex);
+    txBuilder.addSignature(signature);
+
+    const broadcastTx = async () => {
+        try {
+            const result = await cosmosclient.rest.tx.broadcastTx(sdk.rest, {
+                tx_bytes: txBuilder.txBytes(),
+                mode: cosmosclient.rest.tx.BroadcastTxMode.Block,
+            });
+
+            if (result.data.tx_response?.code !== 0) {
+                console.error(result.data.tx_response?.raw_log);
+            }
+            const ret = result.data;
+            instance.publishState("tx_hash", ret.tx_response.txhash);
+        } catch (err) {
+            throw err;
+        } finally {
+            console.log('bradcasted!')
+            instance.data.msgs = [];
+            instance.publishState("body_hex", "");
+            instance.publishState("auth_info_hex", "");
+            instance.publishState("account_long_str", "");
+            instance.triggerEvent("broadcasted");
+        }
+    };
+
+    broadcastTx();
+}
---
+++ src/cosmos_with_keplr/cosmos_create_tx.bubble.js
@@ -0,0 +1,50 @@
+function(instance, properties, context) {
+    const { toHexString, fetchAccount, currentAddressString, sdk, msgs } = instance.data;
+
+    fetchAccount(sdk, currentAddressString).then((account) => {
+        console.log(msgs)
+        const txBody = createTxBody(msgs);
+        const authInfo = createAuthInfo(account);
+        const txBuilder = new cosmosclient.TxBuilder(sdk, txBody, authInfo);
+        const signDoc = txBuilder.signDoc(account.account_number);
+
+        instance.data.txBuilder = txBuilder;
+
+        console.log({ account });
+
+        instance.publishState("body_hex", toHexString(signDoc.body_bytes));
+        instance.publishState("auth_info_hex", toHexString(signDoc.auth_info_bytes));
+        instance.publishState("account_long_str", account.account_number.toString());
+        instance.triggerEvent("tx_created");
+        console.log('tx created')
+    });
+
+    //
+    // subroutine
+    //
+    function createTxBody(msgs) {
+        const txBody = new cosmosclient.proto.cosmos.tx.v1beta1.TxBody({
+            messages: msgs.map((msg) => cosmosclient.codec.instanceToProtoAny(msg)),
+        });
+        return txBody;
+    }
+
+    function createAuthInfo(account) {
+        return new cosmosclient.proto.cosmos.tx.v1beta1.AuthInfo({
+            signer_infos: [
+                {
+                    public_key: account.pub_key,
+                    mode_info: {
+                        single: {
+                            mode: cosmosclient.proto.cosmos.tx.signing.v1beta1.SignMode.SIGN_MODE_DIRECT,
+                        },
+                    },
+                    sequence: account.sequence,
+                },
+            ],
+            fee: {
+                gas_limit: Long.fromString("200000"),
+            },
+        });
+    }
+}
