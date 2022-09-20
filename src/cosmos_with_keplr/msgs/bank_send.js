ac726e5d4af6c2c88a2dfa9a1b0de8b845c69b60
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-09-20 19:51:53 +0900

    feat: bank_send

---
+++ src/cosmos_with_keplr/msgs/bank_send.js
@@ -0,0 +1,16 @@
+function(instance, properties, context) {
+    const { currentAddressString } = instance.data;
+
+    const toAddress = properties.to_address;
+    const amount = properties.amount;
+    const denom = properties.denom;
+
+    const msg = new cosmosclient.proto.cosmos.bank.v1beta1.MsgSend({
+        from_address: currentAddressString,
+        to_address: toAddress,
+        amount: [{ denom: denom, amount: amount }],
+    });
+
+    instance.data.msgs.push(msg);
+    instance.triggerEvent('msg_send_created');
+}
