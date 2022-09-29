238142ed2d36407be73c6c392f262c26be2857c2
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-09-29 14:33:11 +0900

    fix: msgAny

--- src/cosmos_with_keplr/tx_basis/create_msg.js
+++ src/cosmos_with_keplr/tx_basis/create_msg.js
@@ -2,6 +2,7 @@ function(instance, properties, context) {
     const { fromHexString } = instance.data;
     const typeURL = properties.type_url;
     const valueBuf = fromHexString(properties.value);
-    const msg = new cosmosclient.proto.google.protobuf.Any({ type_url: typeURL, value: valueBuf });
+    const msgAny = new cosmosclient.proto.google.protobuf.Any({ type_url: typeURL, value: valueBuf });
+    const msg = cosmosclient.codec.protoAnyToInstance(msgAny);
     instance.data.msgs = [msg];
 }
