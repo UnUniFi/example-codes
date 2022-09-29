6e16583e8a69e672f2da212ab0ccfbaa6ca6e6db
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-09-29 14:23:20 +0900

    feat: msg pass to tx-basis

--- src/cosmos_with_keplr/msgs/bank_msg_send.js
+++ src/cosmos_with_keplr/msgs/bank_msg_send.js
@@ -1,5 +1,5 @@
 function(instance, properties, context) {
-    const { currentAddressString } = instance.data;
+    const { currentAddressString, toHexString } = instance.data;
 
     const toAddress = properties.to_address;
     const amount = properties.amount;
@@ -12,5 +12,8 @@ function(instance, properties, context) {
     });
 
     instance.data.msgs = [msg];
+    const msgAny = cosmosclient.codec.instanceToProtoAny(msg);
+    instance.publishState('msg_send_type_url', msgAny.type_url);
+    instance.publishState('msg_send_value', toHexString(msgAny.value));
     instance.triggerEvent('msg_send_created');
 }
