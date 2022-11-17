4917336e6853be8fc2449c744dd2e928f971118e
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-11-17 21:45:20 +0900

    feat: add log

--- src/ununifi_with_keplr/poc/ununifi_nft_value_calculator.js
+++ src/ununifi_with_keplr/poc/ununifi_nft_value_calculator.js
@@ -1,8 +1,8 @@
 // update
 function(instance, properties, context) {
-    const address = properties.address;
-    const classID = properties.class_id;
-    const nftID = properties.nft_id;
+    const address = properties.address
+    const classID = properties.class_id
+    const nftID = properties.nft_id
     function changeQuinary(num) {
         return num % 2
     }
@@ -17,23 +17,34 @@ function(instance, properties, context) {
         const lastChar = address.slice(-1);
         const lastCharCode = lastChar.charCodeAt(0)
         const userPointType = changeQuinary(getLastNum(lastCharCode))
-        const classLastChar = classID.slice(-1);
+        console.log('address binary: ' + userPointType)
+        const classLastChar = classID.slice(-1)
         const classLastCharCode = classLastChar.charCodeAt(0)
         const classPointType = changeQuinary(getLastNum(classLastCharCode))
+        console.log('class ID binary: ' + classPointType)
         if (userPointType == classPointType) {
             if (nftID) {
-                const nftLastChar = nftID.slice(-1);
+                const nftLastChar = nftID.slice(-1)
                 const nftLastCharCode = nftLastChar.charCodeAt(0)
                 const nftPointType = changeQuinary(getLastNum(nftLastCharCode))
+                console.log('NFT ID binary: ' + nftPointType)
                 if (classPointType == nftPointType) {
+                    console.log('3 arg match')
                     publishState('1000GUU')
-                } else { publishState('100GUU') }
+                } else {
+                    console.log('address & class ID match')
+                    publishState('100GUU')
+                }
             } else {
+                console.log('address & class ID match,  no NFT ID')
                 publishState('100GUU')
             }
-        } else { publishState('1GUU') }
+        } else {
+            console.log('no arg match')
+            publishState('1GUU')
+        }
     } else {
+        console.log('no address or class ID')
         publishState('1GUU')
     }
-
 }
