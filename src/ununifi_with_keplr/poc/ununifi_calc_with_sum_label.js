d141d3f326e6dfa3088fe85a9ca27a7681f4b236
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-11-24 20:19:57 +0900

    feat: calc average

---
+++ src/ununifi_with_keplr/poc/ununifi_calc_with_sum_label.js
@@ -0,0 +1,32 @@
+function(instance, properties, context) {
+    const sumLabelTmp = properties.sum_label
+    const barColor = properties.bar_color
+    let newArray = instance.data.myArray.slice()
+
+    let sumLabel = properties.sum_label
+    let minValue = newArray[newArray.length - 1][1]
+    let sumLabelAnnotation = 0
+    let i = 1
+
+    while (sumLabel > 0 && newArray.length >= i) {
+        if (sumLabel - Number(newArray[newArray.length - i][0]) > 0) {
+            sumLabelAnnotation += Number(newArray[newArray.length - i][0]) * Number(newArray[newArray.length - i][3].replace(instance.data.annotationSuffix, ''))
+        } else {
+            sumLabelAnnotation += sumLabel * Number(newArray[newArray.length - i][3].replace(instance.data.annotationSuffix, ''))
+        }
+        if (minValue > newArray[newArray.length - i][1]) {
+            minValue = newArray[newArray.length - i][1]
+        }
+        newArray[newArray.length - i][2] = instance.data.colorConvert(barColor)
+        sumLabel -= Number(newArray[newArray.length - i][0])
+        i++
+    }
+    while (newArray.length >= i) {
+        newArray[newArray.length - i][2] = instance.data.colorConvert(instance.data.barColor)
+        i++
+    }
+
+    instance.publishState('min_value', minValue.toLocaleString())
+    instance.publishState('ave_annotation', (sumLabelAnnotation / sumLabelTmp).toFixed(2))
+    instance.data.drawChart(newArray)
+}
